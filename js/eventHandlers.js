a308cc3ab6e1318fe5884db47fa34dc71ac786fd
Jørgen Jacobsen <retrojorgen@Jrgens-MacBook-Air.local>
2014-07-30 20:21:56 +0200

    Hide hover on mobile

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -8,13 +8,15 @@
 
     // Make funky changes on scrolling
     $(window).on('scroll', function() {
-      $('.header').css('background-position-y', -(window.scrollY/2));
+      if($('.down-icon').css('font-size') !== '20px') {
+        $('.header').css('background-position-y', -(window.scrollY/2));
+      }
     });
 
     // Scroll down to first section on button click
     $('.scroll-down').on('click', function() {
       $('body, html').animate({
-        scrollTop: (firstSectionOffsetTop+40) + 'px'
+        scrollTop: firstSectionOffsetTop + 'px'
       }, 400);
     });
 
